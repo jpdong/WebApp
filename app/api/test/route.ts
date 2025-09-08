f414c03459efdc8637763165ee8b06d4eefd445d
dongjiangpeng <dongjiangpeng@baidu.com>
2025-09-08 18:54:42 +0800

    fix:add test api

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,4 +1,6 @@
-export function GET(req:Request) {
-    const data = {code:0,message:'hello world'};
+export async function GET(req:Request) {
+    const url = new URL(req.url);
+    const query = url.searchParams.get('query');
+    const data = {code:0,message:'hello world',data:query};
     return Response.json(data);
 }
